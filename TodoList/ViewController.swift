262f542a21878664877ea4dda443ddd77b5b6b3b
Peter-Chong <wlchong@edu.uwaterloo.ca>
2019-10-28 11:50:54 -0400

    Initial Commit

---
+++ TodoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by Weng Liang Chong on 28/10/2019.
+//  Copyright © 2019 Weng Liang Chong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
